4250c003d86f6583bf2167f8d3063e74a619b3da
Brajesh Kumar <brajeshkumar@Brajeshs-MacBook-Pro.local>
2023-03-13 15:39:25 +0530

    first commit'

---
+++ pages/imageArray.tsx
@@ -0,0 +1,17 @@
+import React  from "react";
+ const img = [
+    {
+        images : "./images/download (1).jpeg"
+    },
+    {
+        images : "./images/download (2).jpeg"
+    },
+    {
+        images : "./images/download (3).jpeg"
+    },
+    {
+        images : "./images/download (4).jpeg"
+    }
+
+]
+export default img 
