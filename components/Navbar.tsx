9f172b616bf93c9c126467cf30cc0c4dc9d65a27
race45 <sushantsaurabh098@gmail.com>
2023-03-15 15:12:58 +0530

    pages created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import Link from 'next/link'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -27,11 +28,13 @@ export default function Navbar() {
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
+                  <Link href="/">
                   <img
                     className="hidden lg:block h-8 w-auto"
                     src="avni-logo.png"
                     alt="avniLogo"
                   />
+                  </Link>
                 </div>
                 
               </div>
