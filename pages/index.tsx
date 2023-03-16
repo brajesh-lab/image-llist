4cbfa126590340ab49d9eafd20d0cf9af3a98a3d
race45 <sushantsaurabh098@gmail.com>
2023-03-16 11:49:35 +0530

    code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { Card, Checkbox, Grid } from '@nextui-org/react';
 import ImageList from '@/components/ImageList'
 import Navbar from '@/components/Navbar';
 import Pagination from '@/components/Pagination';
-import Dropdown from '@/components/FilterComponent/LocationHierarchy'
 import Filter from '@/components/FilterComponent/Filter';
 
 
@@ -35,27 +29,6 @@ export default function Home() {
     <Filter />
     <ImageList />
     <Pagination />
-     {/* {img.map((item, index) => (
-     <div className='flex'>
-       <Card css={{ p: "$6", mw: "400px" }}>
-       <Card.Header>
-  
-       < img  key={index} src={item.images} alt="image"   width={500}
-       height={500} />
-       </Card.Header>
-   <Card.Body>
-    Image name  
-   </Card.Body>
-   <Card.Footer>
-   <Checkbox><select name=" " id=""></select> select Image </Checkbox>
-   </Card.Footer>
-
-   </Card>
-   </div>
-     ))} */}
-
-
-  
     </>
   )
 }
